79f97c1365bfccde44caa1b5985cac0f502f392c
Erik Dzul Diaz <erikdzul@gmail.com>
2016-12-01 11:53:30 -0600

    Added ToDoItem

---
+++ XamarinDiplomado/Models/ToDoItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.WindowsAzure.MobileServices;
+using Newtonsoft.Json;
+
+namespace XamarinDiplomado
+{
+	[DataTable("TodoItem")]
+	public class ToDoItem
+	{
+		[JsonProperty("Id")]
+		public string Id { get; set;}
+
+		[JsonProperty("Titulo")]
+		public string Titulo { get; set;}
+
+		[JsonProperty("Descripcion")]
+		public string Descripcion { get; set; }
+
+		[Version]
+		public string Version { get; set;}
+	}
+}
